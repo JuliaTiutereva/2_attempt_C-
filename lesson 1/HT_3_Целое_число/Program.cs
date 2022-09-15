16161be8d1c4207c67cfc916e1a991502aa903fe
Julia Tiutereva <julia.tiutereva@gmail.com>
2022-09-15 08:24:51 +0300

    ДЗ Четное ли Число

---
+++ lesson 1/HT_3_Целое_число/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите числo");
+string Num = Console.ReadLine();
+double D_Num = Convert.ToDouble(Num);
+double quotient = D_Num % 2;
+if (quotient == 0)
+{
+    Console.WriteLine("да");
+}
+else 
+{
+    Console.Write("нет");
+}
